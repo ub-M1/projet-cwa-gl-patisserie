16fe7e4ecd993ca145e163795b1d1c48bcb865cf
Abdul-Qadir SANNY <qadirsanny@gmail.com>
2022-11-30 19:03:53 +0100

    review skeleton

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClientsListComponent } from './pages/admin-side/clients-list/clients-list.component';
+import { OrdersListComponent } from './pages/admin-side/orders-list/orders-list.component';
+import { ProductsListComponent } from './pages/admin-side/products-list/products-list.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './client_side/home/home.component';
-import { CartComponent } from './client_side/cart/cart.component';
-import { ProductDetailsComponent } from './client_side/product-details/product-details.component';
-import { ProductListComponent } from './client_side/product-list/product-list.component';
-import { OrdersComponent } from './client_side/orders/orders.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ClientsListComponent } from './admin/clients-list/clients-list.component';
-import { ProductsListComponent } from './admin/products-list/products-list.component';
-import { OrdersListComponent } from './admin/orders-list/orders-list.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/client-side/home/home.component';
+import { CartComponent } from './pages/client-side/cart/cart.component';
+import { ProductDetailsComponent } from './pages/client-side/product-details/product-details.component';
+import { ProductListComponent } from './pages/client-side/product-list/product-list.component';
+import { OrdersComponent } from './pages/client-side/orders/orders.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 @NgModule({
   declarations: [
