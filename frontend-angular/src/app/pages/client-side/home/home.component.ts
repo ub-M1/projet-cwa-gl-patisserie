9962b001ebc763ffdd0ea088555dfaa3914008a3
ikram <tajri.ikram001@gmail.com>
2022-12-21 20:50:51 +0100

    ajout de la principale

--- frontend-angular/src/app/pages/client-side/home/home.component.ts
+++ frontend-angular/src/app/pages/client-side/home/home.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  // Propriété pour stocker les données du menu
+  menu: any[] = [
+    {
+      name: 'Tarte aux pommes',
+      description: 'Description de la tarte aux pommes',
+      price: 10
+    },
+    {
+      name: 'Gâteau au chocolat',
+      description: 'Description du gâteau au chocolat',
+      price: 10
+    },
+    {
+      name: 'Tarte aux fruits rouges',
+      description: 'Description de la tarte aux fruits rouges',
+      price: 10
+    },
+    {
+      name: 'Gâteau à la vanille',
+      description: 'Description du gâteau à la vanille',
+      price: 15
+    },
+    {
+      name: 'Tarte au citron',
+      description: 'Description de la tarte au citron',
+      price: 10
+    },
+    {
+      name: 'Gâteau  au chocolat blanc',
+      description: 'Description du gâteau  au chocolat blanc',
+      price: 10
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // Méthode pour soumettre le formulaire de contact
+  submitForm(form: any): void {
+    console.log(form.value);
   }
-
 }
--- frontend-angular/src/app/pages/client-side/product-list/product-list.component.ts
+++ frontend-angular/src/app/pages/client-side/product-list/product-list.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
+
+
 export class ProductListComponent implements OnInit {
+  searchTerm = '';
+  selectedProduct: any = null;
+  products: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    // Obtenez les données de produits à partir de votre backend ou de votre fichier de données
+    this.products = [
+      { id: 1, name: 'Croissant' },
+      { id: 2, name: 'Pain au chocolat' },
+      { id: 3, name: 'Tarte aux pommes' },
+      // ...
+    ];
   }
-
+  search() {
+    this.products = this.products.filter(product => product.name.includes(this.searchTerm));
+  }
+  
 }
+ /* ProductList() {
+    this.products = this.products.filter(product => product.name.includes(this.searchTerm));
+  }
+  */
+ 
