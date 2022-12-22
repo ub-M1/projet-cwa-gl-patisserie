97b8937340ae15e01385d69cef4ff2444836ec8e
ZoubirAmina <zoubiramina10@gmail.com>
2022-12-22 15:08:49 +0100

    commiting clientslist

--- frontend-angular/src/app/pages/admin-side/clients-list/clients-list.component.ts
+++ frontend-angular/src/app/pages/admin-side/clients-list/clients-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/User';
+import { UtilisateurSService } from 'src/app/services/UtilisateurService/utilisateur-s.service';
 
 @Component({
   selector: 'app-clients-list',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clients-list.component.scss']
 })
 export class ClientsListComponent implements OnInit {
+  ListU!:User[];
+  subPs!:Subscription;
+  constructor(private User:UtilisateurSService) {
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
+    console.log(this.User.User);
+    this.subPs=this.User.User.subscribe((p:User[])=>{
+      this.ListU=p;
+
+    });
+    /*
+    this.ListU=[
+      {
+        id: 1021,
+        nom: "ZOUBIR",
+        prenom: "Amina",
+        email: "zoubir@gmail.com",
+        username: "amina123",
+        tel:712345678,
+      },
+      {
+        id: 1021,
+        nom: "trinite",
+        prenom: "mombouli",
+        email: "trinite@gmail.com",
+        username: "tm123",
+        tel:712345678,
+      }
+    ]*/
   }
 
 }
