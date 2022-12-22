8e6fa0db16451dd49994320debf0a623f1e06720
Abdul-Qadir SANNY <qadirsanny@gmail.com>
2022-12-22 02:33:57 +0100

    handle product lisy and search

--- frontend-angular/src/app/pages/client-side/home/home.component.ts
+++ frontend-angular/src/app/pages/client-side/home/home.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/Produit';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   // Propriété pour stocker les données du menu
   menu: any[] = [
     {
@@ -40,8 +43,48 @@ export class HomeComponent {
     }
   ];
 
+  produitsList: Product[] = []
+
+  searchterm =""
+
+  searchResult: Product[] = []
+
+  constructor(private apiService: ApiService, private router: Router) {
+    this.getProducts()
+
+   }
+
+
   // Méthode pour soumettre le formulaire de contact
   submitForm(form: any): void {
     console.log(form.value);
+  }
+
+  
+  ngOnInit(): void {
+  }
+
+  getProducts(){
+    this.apiService.getProductsList().subscribe(
+      (response)=>{
+        console.log('response :>> ', response);
+        this.produitsList = response.map( (product: any) =>{
+          let p: Product = new Product(product)
+          p.image = "assets/images/image1.jpg"
+          return p;
+        })
+      },
+      (error) => {
+        console.error(error)
+      }
+    )
+  }
+
+  gotodetail(p:Product){
+    this.router.navigateByUrl('product/'+p._id)
+  }
+
+  productBySearch(){
+    return this.produitsList.filter((p) => p.designation?.toLowerCase().includes(this.searchterm.toLowerCase()))
   }
 }
--- frontend-angular/src/app/pages/client-side/product-list/product-list.component.ts
+++ frontend-angular/src/app/pages/client-side/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/Produit';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,28 +11,50 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class ProductListComponent implements OnInit {
-  searchTerm = '';
-  selectedProduct: any = null;
-  products: any[] = [];
+  produitsList: Product[] = []
 
-  constructor() { }
+  searchterm =""
 
-  ngOnInit(): void {
-    // Obtenez les données de produits à partir de votre backend ou de votre fichier de données
-    this.products = [
-      { id: 1, name: 'Croissant' },
-      { id: 2, name: 'Pain au chocolat' },
-      { id: 3, name: 'Tarte aux pommes' },
-      // ...
-    ];
+  searchResult: Product[] = []
+
+  constructor(private apiService: ApiService, private router: Router) {
+    this.getProducts()
+
+   }
+
+
+  // Méthode pour soumettre le formulaire de contact
+  submitForm(form: any): void {
+    console.log(form.value);
   }
-  search() {
-    this.products = this.products.filter(product => product.name.includes(this.searchTerm));
-  }
+
   
-}
- /* ProductList() {
-    this.products = this.products.filter(product => product.name.includes(this.searchTerm));
+  ngOnInit(): void {
   }
-  */
+
+  getProducts(){
+    this.apiService.getProductsList().subscribe(
+      (response)=>{
+        console.log('response :>> ', response);
+        this.produitsList = response.map( (product: any) =>{
+          let p: Product = new Product(product)
+          p.image = "assets/images/image1.jpg"
+          return p;
+        })
+      },
+      (error) => {
+        console.error(error)
+      }
+    )
+  }
+
+  gotodetail(p:Product){
+    this.router.navigateByUrl('product/'+p._id)
+  }
+
+  productBySearch(){
+    return this.produitsList.filter((p) => p.designation?.toLowerCase().includes(this.searchterm.toLowerCase()))
+  }
+}
+
  
